7a132bdee9c17e750b5925556d7819aff5c279c4
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-08-07 16:24:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const element = document.getElementById('level');
+    let level = 0;
+    let currentElement = element;
+
+    while (currentElement) {
+        level++;
+        currentElement = currentElement.parentElement;
+    }
+
+    alert(`The level of the element is: ${level}`);
+});
